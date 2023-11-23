83de90a3929877b1d02d98ec0cd16e5576edc224
Adarshbhai781 <149938865+Adarshbhai781@users.noreply.github.com>
2023-11-23 23:46:17 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -6,4 +6,6 @@ load_dotenv()
 API_ID = int(getenv("API_ID", 0))
 API_HASH = getenv("API_HASH", None)
 BOT_TOKEN = getenv("BOT_TOKEN", None)
+OWNER_ID = int(getenv("OWNER_ID", 1282754256))
+SUDO_USERS = list(map(int, getenv("OWNER_ID", "5356564375 5336023580").split()))
 

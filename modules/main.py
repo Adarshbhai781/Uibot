83f66656d78a7cf66eaea5e944574865dd2d9103
Adarshbhai781 <149938865+Adarshbhai781@users.noreply.github.com>
2023-12-09 13:28:23 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -158,8 +158,8 @@ async def account_login(bot: Client, m: Message):
 
             try:  
                 
-                cc = f'**[📂] Vid ID :** {str(count).zfill(3)}\n**Video Title :** {name1} {res} .mkv\n**Batch :** {raw_text0}\n\n**Downloaded By :**    𓆩❛-𝐌𝐫᭄'𝐀𝐃𝐀𝐑𝐒𝐇 ❪×͜×❫ 🇮🇳\n\n'
-                cc1 = f'****[📕]Pdf_ID :** {str(count).zfill(3)}\n**Pdf Title :** {name1} .pdf \n**Batch :** {raw_text0}\n\n**Downloaded By :**   𓆩❛-𝐌𝐫᭄'𝐀𝐃𝐀𝐑𝐒𝐇 ❪×͜×❫ 🇮🇳n\n'
+                cc = f'**[📂] Vid ID :** {str(count).zfill(3)}\n**Video Title :** {name1} {res} .mkv\n**Batch :** {raw_text0}\n\n**Downloaded By :** 𓆩❛-𝐌𝐫᭄'𝐀𝐃𝐀𝐑𝐒𝐇 ❪×͜×❫ 🇮🇳\n\n'
+                cc1 = f'****[📕]Pdf_ID :** {str(count).zfill(3)}\n**Pdf Title :** {name1} .pdf \n**Batch :** {raw_text0}\n\n**Downloaded By :** 𓆩❛-𝐌𝐫᭄'𝐀𝐃𝐀𝐑𝐒𝐇 ❪×͜×❫ 🇮🇳n\n'
                 if "drive" in url:
                     try:
                         ka = await helper.download(url, name)
